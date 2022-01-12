348323637b79eb56bf0892ce316359b0107784be
Kajal Jain <68593073+22kj@users.noreply.github.com>
2022-01-11 17:29:25 -0800

    Add files via upload

---
+++ img/test.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+	<style type="text/css">
+		.imgpc{
+
+			width: 70px;
+			height: 70px;
+		}
+
+		.imgpc:hover .dropdown-content {
+    		display: block;
+		}
+		
+		.dropdown:hover .dropdown-content {
+    		display: inline-block;
+		}
+		
+		
+		.dropdown {
+			padding-left: 35px;
+    		position: relative;
+    		display: inline-block;
+    		max-width: 150px;
+		}
+
+		.dropdown-content a {
+    		color: white;
+    		padding: 12px 16px;
+    		text-decoration: none;
+    		display: block;
+		}
+		.dropdown-content {
+		    display: none;
+		    position: absolute;
+			font-color:white;    
+
+			background-color:rgba(255,255,255,0.5); 
+			border-radius:8px;
+			border-color:black;
+    
+		    min-width: 60px;
+		    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    		z-index: 1;
+		}
+
+	</style>
+
+
+</head>
+<body style="background-image: url(digital-solutions.jpg);">
+
+<?php
+
+$i = 0;
+
+while($i<5){
+	echo  "
+	
+<div class=\"dropdown\" >
+
+<img class=\"imgpc\" src=\"pcicon.png\"><br>
+
+<div class=\"dropdown-content\">";
+$j=0;
+while($j<5){
+	echo "<label>lj</label><br>";
+	$j++;
+}
+
+echo "
+</div>
+</div>
+	
+	";
+	$i++;
+	}
+?>
+
+
+
+</body>
+</html>
